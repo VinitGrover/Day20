d616cc486bdc02af28781673d7c929bbb5ef9e27
VinitGrover <vinitgrover255@gmail.com>
2022-06-19 21:19:03 +0530

    Added Test Password

--- src/main/java/com/bridgelabz/RegexPassword.java
+++ src/main/java/com/bridgelabz/RegexPassword.java
@@ -3,23 +3,11 @@ package com.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class RegexPassword { 
+public class RegexPassword {
+    Pattern pattern = Pattern.compile("^[0-9a-zA-Z]{8,}$");
 
-    public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("^[0-9a-zA-Z]{8,}$");
-
-        System.out.println("Enter minimum 8 digit password ");
-
-        System.out.println("Enter at least 8 digit password ");
-        Scanner userInput = new Scanner(System.in);
-        String password = userInput.nextLine();
-
-        Matcher matcher = pattern.matcher(password);
-
-        if (matcher.matches() == false) {
-            System.out.println("Invalid Password");
-        } else {
-            System.out.println("Valid Password");
-        }
+    boolean checkPassword(String name) {
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
     }
 }
